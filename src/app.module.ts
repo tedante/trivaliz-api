3a01cd55f6f418ea9e67a6ce83fe5aef34b12a2c
Teddy K <teddykoerniadi@gmail.com>
2024-12-23 15:24:07 +0700

    Add helmet and rate limiter middleware for enhanced security and request management

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,23 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 import { DATABASE } from './common/config';
-
-console.log(DATABASE);
-
+import { RateLimiterModule, RateLimiterGuard } from 'nestjs-rate-limiter'
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
     MongooseModule.forRoot(DATABASE.URI, { dbName: DATABASE.DB_NAME }),
     UserModule,
+    RateLimiterModule.register({
+      points: 10, // Number of points
+      duration: 1, // Per second(s)
+    }),
+    RateLimiterModule
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, {
+    provide: APP_GUARD,
+    useClass: RateLimiterGuard,
+  }],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CLIENT_URL, PORT } from './common/config';
+import helmet from 'helmet';
+import { RateLimiterModule } from 'nestjs-rate-limiter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +10,8 @@ async function bootstrap() {
   app.enableCors({
     origin: CLIENT_URL.split(";").map((el) => el.trim()),
   });
+
+  app.use(helmet());
 
   await app.listen(PORT);
 }
