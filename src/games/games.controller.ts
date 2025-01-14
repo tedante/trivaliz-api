7556842d1ba60d4c59cdd7da344d038c5e14ed2f
Fachri Hawari <fachri.hawari@gmail.com>
2025-01-14 10:41:04 +0700

    add rankings to game find by id

--- src/games/games.controller.ts
+++ src/games/games.controller.ts
@@ -2,10 +2,11 @@ import { Controller, Param, Get, Post, Req, Body } from '@nestjs/common';
 import { GamesService } from './games.service';
 import { Request } from 'express';
 import { StartGameDto } from './games.dto';
+import { UsersService } from 'src/users/users.service';
 
 @Controller('games')
 export class GamesController {
-  constructor(private readonly gamesService: GamesService) {}
+  constructor(private readonly gamesService: GamesService, private readonly usersService: UsersService) {}
 
   @Post('start')
   async startGame(@Req() request: Request, @Body() body: StartGameDto) {
@@ -29,6 +30,20 @@ export class GamesController {
 
   @Get(':gameId')
   async getGame(@Param('gameId') gameId: string) {
-    return this.gamesService.findGame(gameId);
+    const game = await this.gamesService.findGame(gameId);
+
+    const playerIds = Object.keys(game.players);
+    const players = await this.usersService.findByIds(playerIds);
+
+    const rankings = game.rankings.map((ranking) => ({
+      playerId: ranking.playerId,
+      score: ranking.score,
+      player: players.find((player) => player.id === ranking.playerId),
+    }));
+
+    return {
+      ...game,
+      rankings,
+    };
   }
 }
